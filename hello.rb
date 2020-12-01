52e83197895fcb7a23e2baa8c4673bd3ae8598bb
Alejandro Contreras <kdta15@outlook.com>
2020-12-01 13:20:04 -0600

    changes

--- hello.rb
+++ hello.rb
@@ -1,1 +1,12 @@
-puts "hello"
+def bubble_sort(arr)
+  arr.each do
+    arr.each_with_index do |_num, index|
+      if arr[index + 1].nil? == false
+        arr[index], arr[index + 1] = arr[index + 1], arr[index] if arr[index] > arr[index + 1]
+      end
+    end
+  end
+  print arr
+end
+
+bubble_sort([4, 3, 78, 2, 0, 2])
